630f199b88d3d092c543d57b66fcbd6f81a10cb7
qurquhart <quintonurquhart@gmail.com>
2020-09-27 11:41:31 -0700

    base system done, add post scripts next

---
+++ img_poster.py
@@ -0,0 +1,117 @@
+import imageio
+import os, sys
+from PIL import Image
+import re
+from logger import Logger, creds
+import shutil
+
+#----------------------------------------------------#
+# Logging Setup
+#
+# log.text('test log.text()', send_mail=False)
+# error.text('test error.text()', send_mail=True)
+#
+#----------------------------------------------------#
+
+email_list = ['quintonurquhart@gmail.com']
+
+log = Logger("activity.txt", 
+             "img_poster",
+             gmail_email=creds('gmail_email'),
+             gmail_password=creds('gmail_password'),
+             email_list=email_list,
+             email_subject="img_poster error log")
+
+error = Logger("error.txt", 
+             "img_poster",
+             gmail_email=creds('gmail_email'),
+             gmail_password=creds('gmail_password'),
+             email_list=email_list,
+             email_subject="img_poster error log")
+
+
+#----------------------------------------------------#
+# /Logging Setup
+#----------------------------------------------------#
+
+#----------------------------------------------------#
+# Directories
+#----------------------------------------------------#
+
+def move_it(base_directory, final_directory):
+
+    try:
+        shutil.move(file_location, f'{base_directory}{final_directory}')
+        log.text(f'Moved {file_location} to {base_directory}{final_directory}.')
+    except:
+        error.text(f'Unable to move {file_location} to {base_directory}{final_directory}')
+
+
+def create_dir(base_directory, directory_to_be):
+
+    if not os.path.isdir(f'{base_directory}{directory_to_be}'):
+        
+        log.text(f'{base_directory}{directory_to_be} directory not found.')
+        try:
+            os.makedirs(os.path.dirname(f'{base_directory}{directory_to_be}'), exist_ok=True)
+            log.text(f'Created {base_directory}{directory_to_be} directory.')
+        except Exception as ex:
+            error.text(f'Unable to create {base_directory}{directory_to_be} directory! - {ex}')
+
+
+base_directory = "./img/"
+invalid_directory = "invalid/"
+posted_directory = "posted/"
+
+create_dir(base_directory, invalid_directory)
+create_dir(base_directory, posted_directory)
+
+first_file = os.listdir(base_directory)[0]
+if first_file == 'invalid':
+    log.text('Base directory empty. Exiting.')
+    exit()       
+
+file_location = f"{base_directory}{first_file}"
+
+log.text(f'First file in base directory: {first_file}')
+
+#----------------------------------------------------#
+# /Directories
+#----------------------------------------------------#
+
+#----------------------------------------------------#
+# Format Check
+#----------------------------------------------------#
+
+def file_type_check(file_location):
+    
+    file_noextension, file_type = os.path.splitext(file_location)
+    filename = os.path.basename(file_noextension)
+
+    acceptable_filetypes = [".mp4",".gif",".jpg", ".png"]
+
+    if file_type in acceptable_filetypes:
+        log.text(f"Acceptable file type: {file_type}")
+        return filename, file_location
+    else:
+        error.text(f"Unacceptable file type: {file_type}.  Acceptable file types include {acceptable_filetypes}")   
+        
+
+post_title, file_to_post = file_type_check(file_location)
+
+#----------------------------------------------------#
+# /Format Check
+#----------------------------------------------------#
+
+#----------------------------------------------------#
+# Post File
+#----------------------------------------------------#
+
+if file_to_post:
+    log.text(f'this is the post title: {post_title}')
+    log.text(f'this is the post title: {file_to_post}')
+    # post it
+
+    move_it(base_directory, posted_directory)
+else:
+    move_it(base_directory, invalid_directory)
---
+++ logger.py
@@ -0,0 +1,120 @@
+from datetime import datetime
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
+import ssl
+import re
+
+
+def creds(key):
+    credentials = open("logger.config")
+    found = 0
+    for line in credentials:
+        search = re.findall(f'{key}=(.*)',line)
+        if search:
+            found += 1
+            return(search[0])
+    if found == 0:
+        return "key not found"
+
+
+class Logger:
+    '''creates log files/entries
+    log = Logger("log.txt", "LOG TEST")
+    log.text("Message to log.")
+    print last n lines of text
+    log.debug(int)
+    '''
+
+    def __init__(self, filename, logging_header, gmail_email = None, gmail_password = None, email_list = None, email_subject = None):
+        self.filename = filename
+        self.logging_header = logging_header
+        self.gmail_email = gmail_email
+        self.gmail_password = gmail_password
+        self.email_list = email_list
+        self.email_subject = email_subject
+
+
+    def text(self, log_text, send_mail=False,):
+        '''print string argument to log.filename'''
+
+        log_line = (f"{datetime.strftime(datetime.now(),'[ %m-%d-%Y %I:%M:%S %p ]')} "\
+                    f"[{self.logging_header}] {log_text} \r")
+
+        print(log_line)
+        os.makedirs(os.path.dirname('logs/'), exist_ok=True)
+        log_file = open(f"logs/{self.filename}", 'a+')
+        log_file.write(log_line)
+        log_file.close()
+
+        if send_mail == True:
+            email_body = log_line
+            email_html = f"<p>{log_line}</p>"
+
+            sender_email = self.gmail_email
+            password = self.gmail_password
+
+            email_list = self.email_list
+
+
+            for receiver_email in email_list:
+                message = MIMEMultipart("alternative")
+                message["Subject"] = self.email_subject
+                message["From"] = sender_email
+                message["To"] = receiver_email
+
+                # Create the plain-text and HTML version of your message
+                text = f"""\
+                {email_body}
+                Updated {datetime.now()}"""
+                html = f"""\
+                <html>
+                    <body>
+                    {email_html}
+                    </body>
+                </html>
+                """
+
+                # Turn these into plain/html MIMEText objects
+                part1 = MIMEText(text, "plain")
+                part2 = MIMEText(html, "html")
+
+                # Add HTML/plain-text parts to MIMEMultipart message
+                # The email client will try to render the last part first
+                message.attach(part1)
+                message.attach(part2)
+
+                # Create secure connection with server and send email
+                context = ssl.create_default_context()
+                with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+                    server.login(sender_email, password)
+                    server.sendmail(
+                        sender_email, receiver_email, message.as_string()
+                    )
+
+
+
+
+    def debug(self, lines, hide_errors=False):
+        '''print string argument to log.filename'''
+
+        if not os.path.isdir("logs/"):
+            print("[ DEBUG ] Logs directory not found.")
+        else:
+            if not os.path.isfile(f"logs/{self.filename}"):
+                print("[ DEBUG ] Log file not found.")
+            else:
+
+                log_file = open(f"logs/{self.filename}", 'r')
+                log_lines = log_file.readlines()
+
+                if len(log_lines) >= lines:
+                    for line in reversed(range(lines)):
+                        print((f"[ DEBUG ] {log_lines[-(line+1)]}").rstrip('\n'))
+                else:
+                    for line in reversed(range(len(log_lines))):
+                        print((f"[ DEBUG ] {log_lines[-(line+1)]}").rstrip('\n'))
+                    if hide_errors is False:
+                        print(f'[ DEBUG ] Unable to print {lines-len(log_lines)}/{lines}'\
+                              f' requested lines. Lines in file: {len(log_lines)}')
